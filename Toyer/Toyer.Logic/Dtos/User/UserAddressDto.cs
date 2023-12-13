0d9d8a5a01701f0724b35a3467efd2ba8809d315
AdrianBajczyk <adrian.bajczyk@gmail.com>
2023-12-13 14:26:45 +0100

    implementing data annotations to UserPersonalInfoDto and UserAddressDto

--- Toyer/Toyer.Logic/Dtos/User/UserAddressDto.cs
+++ Toyer/Toyer.Logic/Dtos/User/UserAddressDto.cs
@@ -1,10 +1,21 @@
-﻿namespace Toyer.Logic.Dtos.User;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Toyer.Logic.Dtos.User;
 
 public class UserAddressDto
 {
+    [StringLength(30, ErrorMessage = "Street cannot exceed 30 characters")]
     public string? Street { get; set; }
+
+    [StringLength(30, ErrorMessage = "Street cannot exceed 30 characters")]
     public string? City { get; set; }
+
+    [StringLength(30, ErrorMessage = "State cannot exceed 30 characters")]
     public string? State { get; set; }
+
+    [StringLength(6, ErrorMessage = "Postal code cannot exceed 10 characters")]
     public string? PostalCode { get; set; }
+
+    [StringLength(30, ErrorMessage = "Country cannot exceed 30 characters")]
     public string? Country { get; set; }
 }
--- Toyer/Toyer.Logic/Dtos/User/UserPersonalInfoDto.cs
+++ Toyer/Toyer.Logic/Dtos/User/UserPersonalInfoDto.cs
@@ -1,10 +1,24 @@
-﻿namespace Toyer.Logic.Dtos.User;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Toyer.Logic.Dtos.User;
 
 public class UserPersonalInfoDto
 {
+    [StringLength(20, ErrorMessage = "First name cannot exceed 20 characters")]
     public string? Name { get; set; }
+
+    [StringLength(20, ErrorMessage = "Surname cannot exceed 20 characters")]
     public string? Surname { get; set; }
+
+    [DataType(DataType.EmailAddress)]
+    [EmailAddress(ErrorMessage = "Invalid email address")]
+    // https://learn.microsoft.com/en-us/aspnet/core/mvc/models/validation?view=aspnetcore-8.0
     public string? Email { get; set; }
-    public string? PhoneNumber { get; set; }
+
+    [DataType(DataType.Date)]
+    [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
     public DateOnly? BirthDate { get; set; }
+
+    [RegularExpression(@"^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$", ErrorMessage = "Invalid phone number format")]
+    public string? PhoneNumber { get; set; }
 }
