668036ab1ed9285eac74d3bbd5313f3adaac8504
AdrianBajczyk <adrian.bajczyk@gmail.com>
2023-12-29 11:28:15 +0100

    order dtos implemented

---
+++ Toyer/Toyer.Logic/Dtos/Order/OrderCreateDto.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Toyer.Logic.Dtos.Order;
+
+public record OrderDto
+{
+    [Required(ErrorMessage = "The id of order is required.")]
+    public int Id { get; set; }
+}
---
+++ Toyer/Toyer.Logic/Dtos/Order/OrderDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Toyer.Logic.Dtos.Order;
+
+public record OrderCreateDto
+{
+    [Required(ErrorMessage = "The name is required.")]
+    [StringLength(20, ErrorMessage = "Login cannot exceed 20 characters.")]
+    [RegularExpression("^[A-Z][a-z]*$", ErrorMessage = "First letter has to be uppercase, the rest have to be lower case.")]
+    public string Name { get; set; }
+
+    [Required(ErrorMessage = "The description is required.")]
+    [StringLength(60, ErrorMessage = "Description cannot exceed 60 characters.")]
+    [RegularExpression(@"(?:^[A-Z]|[.!?]\s+[A-Z])", ErrorMessage = "Check if every beginning of sentence is followed by capital letter.")]
+    public string Description { get; set; }
+
+    [Required(ErrorMessage = "The device type is required.")]
+    [Display(Name = "Device Type IDs")]
+    public List<int> DeviceTypeId { get; set; } 
+}
