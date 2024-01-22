4d059768f152c1d1f434f5f4a692b4ed34fad07a
AdrianBajczyk <adrian.bajczyk@gmail.com>
2024-01-22 15:34:32 +0100

    List component wrapper (generic, wraps content into <ul> basicly need to pass array and map elem to <li>)

---
+++ ToyerClient/components/List.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { type ReactNode } from 'react';
+
+type ListProps<T> = {
+  items: T[];
+  renderItem: (item: T) => ReactNode;
+};
+
+export function List<T>({ items, renderItem }: ListProps<T>) {
+  return <ul>{items.map(renderItem)}</ul>;
+}
