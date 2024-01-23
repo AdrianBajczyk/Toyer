449ae99893032112c418be8b90845d9d1735680f
AdrianBajczyk <adrian.bajczyk@gmail.com>
2024-01-23 12:24:57 +0100

    Enxanced Form element to be able to access/refer to its method outside component

--- ToyerClient/components/Form.tsx
+++ ToyerClient/components/Form.tsx
@@ -1,18 +1,34 @@
-import React, { type FormEvent, type ComponentPropsWithoutRef } from "react";
+import React, { type FormEvent, type ComponentPropsWithoutRef, useRef, useImperativeHandle, forwardRef } from "react";
 
 type FormProps = ComponentPropsWithoutRef<'form'> & {
     onSave: (value: unknown) => void;
 };
 
-export default function Form(props: FormProps){
+export type FormHandle = {
+    clear: () => void;
+};
+
+ const Form = forwardRef<FormHandle, FormProps>( function Form({onSave, children, ...otherProps}, ref){
+
+    const form = useRef<HTMLFormElement>(null)
+
+    useImperativeHandle(ref, () => {
+        return { 
+            clear() {
+                form.current?.reset();
+            }
+        }
+    });
 
     function handleSubmit(event: FormEvent<HTMLFormElement>){
         event.preventDefault();
 
         const formData = new FormData(event.currentTarget);
         const data = Object.fromEntries(formData);
-        props.onSave(data);
+        onSave(data);
     }
 
-    return <form onSubmit={handleSubmit} {...props}>{props.children}</form>
-}
+    return <form onSubmit={handleSubmit} ref={form} {...otherProps}>{children}</form>
+})
+
+export default Form;
