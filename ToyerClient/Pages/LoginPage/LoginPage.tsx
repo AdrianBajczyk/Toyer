f4817fad4891ec4c497a181e5dbc11401e91c7a1
AdrianBajczyk <adrian.bajczyk@gmail.com>
2024-01-23 11:48:19 +0100

    cleaning starter code + basic routing implemnted into App.tsx

--- ToyerClient/Pages/LoginPage/LoginPage.tsx
+++ ToyerClient/Pages/LoginPage/LoginPage.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 
-interface LoginPageProps{
-    
-}
+interface LoginPageProps {}
 
-export default function LoginPage(props: LoginPageProps){
-    return <div>LoginPage</div>
+export default function LoginPage(props : LoginPageProps) {
+  return <div>LoginPage</div>;
 }
--- ToyerClient/src/App.tsx
+++ ToyerClient/src/App.tsx
@@ -1,10 +1,23 @@
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LoginPage from "../Pages/LoginPage/LoginPage.tsx";
+import HomePage from "../Pages/HomePage/HomePage.tsx";
+import DevicePage from "../Pages/DevicePage/DevicePage.tsx";
 
 function App() {
-
   return (
-    <></>
-  )
-}
+    <div className="App">
+      <BrowserRouter>
 
-export default App
+
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/device/:id" element={<DevicePage />} />
+        </Routes>
+
+        
+      </BrowserRouter>
+    </div>
+  );
+}
+export default App;
