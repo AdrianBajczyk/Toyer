8da39bb4bb665ded172b65c157ae98fb7e851ba7
AdrianBajczyk <adrian.bajczyk@gmail.com>
2024-01-24 11:09:41 +0100

    1. JSONObject type introduced 2. HTTP fetch POST generic helper implemented

--- ToyerClient/Pages/DevicePage/DevicePage.tsx
+++ ToyerClient/Pages/DevicePage/DevicePage.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 interface DevicePageProps {}
 
 export default function DevicePage(props: DevicePageProps) {
--- ToyerClient/Utils/http.ts
+++ ToyerClient/Utils/http.ts
@@ -1,3 +1,13 @@
+
+import {JSONObject, JSONArray} from "./JsonType.ts"
+
+type RequestOptions = {
+    method: string,
+    headers: { 'Content-Type' : string},
+    body: string
+}
+
+
 export async function get<T>(url: string){
     const response = await fetch(url)
 
@@ -7,4 +17,25 @@ export async function get<T>(url: string){
 
     const data = await response.json() as unknown;
     return data as T;
+}
+
+export async function post<T>(url: string, reqBody: JSONObject){
+
+    const stringBody : string = JSON.stringify({body: reqBody});
+
+    const requestOptions : RequestOptions = {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: stringBody
+    }
+
+    const response = await fetch(url, requestOptions)
+
+    if (!response.ok){
+        throw new Error(`Faied to fetch data. ${response.status} ${response.statusText}.`);
+    }
+
+    const data = await response.json() as unknown;
+    return data as T;
+
 }
