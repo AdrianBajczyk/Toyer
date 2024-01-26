4f8d0e27daef13934b979e6fed6a5c45eedb3355
AdrianBajczyk <adrian.bajczyk@gmail.com>
2024-01-26 09:52:45 +0100

    back button with relative path added to DeviceDetails page

--- ToyerClient/Pages/DeviceDetails/DeviceDetails.tsx
+++ ToyerClient/Pages/DeviceDetails/DeviceDetails.tsx
@@ -1,8 +1,15 @@
-import React from "react";
+import { useNavigate } from "react-router";
+import Button from "../../Components/Button.tsx"
+
+
 
 function DeviceDetials(){
+
+    const navigate = useNavigate()
+
     return<>
     <p>SomeDetails</p>
+    <Button element='button' onClick={() => navigate("..", { relative: "path" })}>Back</Button>
     </>
 }
 
--- ToyerClient/components/Button.tsx
+++ ToyerClient/components/Button.tsx
@@ -1,16 +1,18 @@
-import React, { type ComponentPropsWithoutRef } from "react";
+import React, { ReactNode, type ComponentPropsWithoutRef, Children } from "react";
 
 type ButtonProps = {
     element: 'button';
+    children: ReactNode;
 } & ComponentPropsWithoutRef<'button'>
 
 type AnchorProps = {
     element: 'anchor';
+    children: ReactNode;
 } & ComponentPropsWithoutRef<'a'>
 
 export default function Button(props: ButtonProps | AnchorProps){
 if(props.element === 'anchor'){
-    return <a {...props}></a>
+    return <a {...props}>{props.children}</a>
 } 
-return <button {...props}></button>
+return <button {...props}>{props.children}</button>
 }
