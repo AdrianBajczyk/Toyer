84e3a897dfd78fe335acfd6684741546e6f645e9
AdrianBajczyk <adrian.bajczyk@gmail.com>
2024-02-05 11:50:03 +0100

    role List to IList cast bug fix

--- ToyerServer/Toyer.Logic/Services/Repositories/Classes/SqlTokenRepository.cs
+++ ToyerServer/Toyer.Logic/Services/Repositories/Classes/SqlTokenRepository.cs
@@ -25,7 +25,7 @@ public class SqlTokenRepository(ITokenService tokenService, UsersDbContext users
         if (userTokens is null || userTokens.RefreshToken != refreshToken || userTokens.RefreshTokenExpiryTime <= DateTime.Now) throw new AuthenticationException();
 
         var user = await _userRepository.GetUserByIdAsync(userId);
-        var role = principal.Claims.SingleOrDefault(c => c.Type == ClaimTypes.Role)!.Value ?? throw new AuthenticationException();
+        var role = (IList<string>)principal.Claims.Where(c => c.Type == ClaimTypes.Role)!.ToList() ?? throw new AuthenticationException();
 
         var newAccessToken = _tokenService.GenerateAccessToken(user, role);
         var newRefreshToken = _tokenService.GenerateRefreshToken();
