854823132601fc627feb431356acb2c61f65edaa
AdrianBajczyk <adrian.bajczyk@gmail.com>
2024-02-16 10:02:29 +0100

    FRONTEND: devices cards expands and stop on click

--- ToyerClient/Components/DevicesList/DevicesList.jsx
+++ ToyerClient/Components/DevicesList/DevicesList.jsx
@@ -1,29 +1,58 @@
+import React, { useState } from "react";
 import classes from "./DevicesList.module.css";
 
-
 function DevicesList({ deviceTypes }) {
+  const [expandedCardId, setExpandedCardId] = useState(null);
+  const [hoverAllowed, setHoverAllowed] = useState(true);
+
+  const handleContainerHover = () => {
+    if (hoverAllowed) {
+      setHoverAllowed(false);
+      setTimeout(() => {
+        setHoverAllowed(true);
+      }, 700);
+    }
+  };
+
+  const handleContainerClick = (deviceId) => {
+    if (expandedCardId === deviceId) {
+      setExpandedCardId(null);
+    } else if (hoverAllowed) {
+      setExpandedCardId(deviceId);
+    }
+  };
 
   return (
     <>
-      {console.log(deviceTypes)}
       <section className={classes.cardsBox}>
-        {deviceTypes.map((device) => (
-          <div key={`deviceType-${device.id}`}
-            className={classes.cardContainer}
-
-          >
-            <div className={classes.card}>
-              <span
-                className={classes.cardFront}
-                style={{ backgroundImage: `url(${device.imageUrl})` }}
-              />
-              <span className={classes.cardBack}>
-                <h3>{device.name}</h3>
-                <p>{device.description}</p>
-              </span>
+        {deviceTypes.map((device) => {
+          const isExpanded = expandedCardId === device.id;
+          return (
+            <div
+              key={`deviceType-${device.id}`}
+              className={`${classes.cardContainer} ${
+                isExpanded ? classes.cardContainerExpanded : ""
+              }`}
+              onClick={() => handleContainerClick(device.id)}
+              onMouseEnter={handleContainerHover}
+            >
+              <div
+                className={`${classes.card} ${
+                  isExpanded ? classes.cardFlipped : ""
+                }`}
+              >
+                <span
+                  className={classes.cardFront}
+                  style={{ backgroundImage: `url(${device.imageUrl})` }}
+                />
+                <span className={classes.cardBack}>
+                  <h3>{device.name}</h3>
+                  <p>{device.description}</p>
+                </span>
+              </div>
             </div>
-          </div>
-        ))}
+          );
+        })}
       </section>
     </>
   );
