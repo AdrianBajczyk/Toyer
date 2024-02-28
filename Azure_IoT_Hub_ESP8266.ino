d1d8002614803dd0538dd7bc8b64407003ef3afb
adrian.bajczyk <adrian.bajczyk@gmail.com>
2024-02-28 10:11:24 +0000

    This is code allowind to program ESP8266 as Led_Device

---
+++ Azure_IoT_Hub_ESP8266.ino
@@ -0,0 +1,358 @@
+
+// C99 libraries
+#include <cstdlib>
+#include <stdbool.h>
+#include <string.h>
+#include <time.h>
+
+// Libraries for MQTT client, WiFi connection and SAS-token generation.
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <WiFiClientSecure.h>
+#include <base64.h>
+#include <bearssl/bearssl.h>
+#include <bearssl/bearssl_hmac.h>
+#include <libb64/cdecode.h>
+#include <ESP8266WebServer.h>
+#include <EEPROM.h>
+
+// Azure IoT SDK for C includes
+#include <az_core.h>
+#include <az_iot.h>
+#include <azure_ca.h>
+
+// Additional sample headers
+#include "iot_configs.h"
+
+// When developing for your own Arduino-based platform,
+// please follow the format '(ard;<platform>)'.
+#define AZURE_SDK_CLIENT_USER_AGENT "c%2F" AZ_SDK_VERSION_STRING "(ard;esp8266)"
+
+// Utility macros and defines
+#define LED_PIN 2
+#define sizeofarray(a) (sizeof(a) / sizeof(a[0]))
+#define ONE_HOUR_IN_SECS 3600
+#define NTP_SERVERS "pool.ntp.org", "time.nist.gov"
+#define MQTT_PACKET_SIZE 1024
+
+// Translate iot_configs.h defines into variables used by the sample
+// static const char* ssid = IOT_CONFIG_WIFI_SSID;
+// static const char* password = IOT_CONFIG_WIFI_PASSWORD;
+static const char* host = IOT_CONFIG_IOTHUB_FQDN;
+static const char* device_id = IOT_CONFIG_DEVICE_ID;
+static const char* device_key = IOT_CONFIG_DEVICE_KEY;
+static const int port = 8883;
+static const char* ap_ssid = IOT_CONFIG_WIFI_AP_SSID;
+static const char* ap_password = IOT_CONFIG_WIFI_AP_PASSWORD;
+
+//Structure to collect data from EEPROM
+struct settings {
+  char ssid[30];
+  char password[30];
+} user_wifi = {};
+
+// Memory allocated for the sample's variables and structures.
+static WiFiClientSecure wifi_client;
+static X509List cert((const char*)ca_pem);
+static PubSubClient mqtt_client(wifi_client);
+static az_iot_hub_client client;
+static char sas_token[200];
+static uint8_t signature[512];
+static unsigned char encrypted_signature[32];
+static char base64_decoded_device_key[32];
+
+//Create HTTP server object on port 80
+ESP8266WebServer server(80);
+
+// Arduino setup and loop main functions.
+
+void setup()
+{
+  pinMode(LED_PIN, OUTPUT);
+  establishConnection();
+}
+
+void loop()
+{
+    // Check if connected, reconnect if needed.
+    if (!mqtt_client.connected() && WiFi.getMode() == 1){ initializeCloudConnection(); }
+
+  // MQTT loop must be called to process Device-to-Cloud and Cloud-to-Device.
+  server.handleClient();
+  mqtt_client.loop();
+  delay(500);
+}
+
+// Auxiliary functions
+
+static void establishConnection()
+{
+  connectToWiFi();
+
+  if (WiFi.getMode() == 1) { initializeCloudConnection(); }
+
+  digitalWrite(LED_PIN, LOW);
+}
+
+static void initializeCloudConnection(){
+
+  initializeTime();
+  printCurrentTime();
+  initializeClients();
+
+  // The SAS token is valid for 1 hour by default in this sample.
+  // After one hour the sample must be restarted, or the client won't be able
+  // to connect/stay connected to the Azure IoT Hub.
+  if (generateSasToken(sas_token, sizeofarray(sas_token)) != 0)
+  {
+    Serial.println("Failed generating MQTT password");
+  }
+  else
+  {
+    connectToAzureIoTHub();
+  }
+
+}
+
+static void connectToWiFi()
+{
+   Serial.begin(115200);
+
+  Serial.println("Reading data from EEPROM.");
+  EEPROM.begin(sizeof(struct settings) );
+  EEPROM.get( 0, user_wifi );
+  Serial.println("Succeed.");
+  
+  Serial.println("Trying to connect by stationary mode");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(user_wifi.ssid, user_wifi.password);
+  
+  byte tries = 0;
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(1000);
+    if (tries++ > 10) {
+      Serial.println("Connection failed. Switching to access point mode.");
+      WiFi.mode(WIFI_AP);
+      //ip and gateway is setup by default to esp device ip=gatewayip=192.168.4.1
+      WiFi.softAP(ap_ssid, ap_password);
+      break;
+    }
+  }
+  server.on("/",  handlePortal);
+  server.begin();
+
+
+  if (WiFi.getMode() == 1){
+    Serial.println("Connected by stationary mode.");
+  } else {
+    Serial.println("Connected by access point mode.");
+    Serial.println("Local IP: 192.168.4.1");
+  }
+}
+
+void handlePortal() {
+
+  if (server.method() == HTTP_POST) {
+
+    strncpy(user_wifi.ssid,     server.arg("ssid").c_str(),     sizeof(user_wifi.ssid) );
+    strncpy(user_wifi.password, server.arg("password").c_str(), sizeof(user_wifi.password) );
+    user_wifi.ssid[server.arg("ssid").length()] = user_wifi.password[server.arg("password").length()] = '\0';
+    EEPROM.put(0, user_wifi);
+    EEPROM.commit();
+
+    server.send(200,   "text/html",  "<!doctype html><html lang='en'><head><meta charset='utf-8'><meta name='viewport' content='width=device-width, initial-scale=1'><title>Toyer Device Wifi Setup</title><style>*,::after,::before{box-sizing:border-box;}body{margin:0;font-family:'Segoe UI',Roboto,'Helvetica Neue',Arial,'Noto Sans','Liberation Sans';font-size:1rem;font-weight:400;line-height:1.5;color:#212529;background-color:#f5f5f5;}.form-control{display:block;width:100%;height:calc(1.5em + .75rem + 2px);border:1px solid #ced4da;}button{border:1px solid transparent;color:#fff;background-color:#007bff;border-color:#007bff;padding:.5rem 1rem;font-size:1.25rem;line-height:1.5;border-radius:.3rem;width:100%}.form-signin{width:100%;max-width:400px;padding:15px;margin:auto;}h1,p{text-align: center}</style> </head> <body><main class='form-signin'> <h1>Wifi Setup</h1> <br/> <p>Your settings have been saved successfully!<br />Please restart the device.</p></main></body></html>" );
+  } else {
+
+    server.send(200,   "text/html", "<!doctype html><html lang='en'><head><meta charset='utf-8'><meta name='viewport' content='width=device-width, initial-scale=1'><title>Toyer Device Wifi Setup</title> <style>*,::after,::before{box-sizing:border-box;}body{margin:0;font-family:'Segoe UI',Roboto,'Helvetica Neue',Arial,'Noto Sans','Liberation Sans';font-size:1rem;font-weight:400;line-height:1.5;color:#212529;background-color:#f5f5f5;}.form-control{display:block;width:100%;height:calc(1.5em + .75rem + 2px);border:1px solid #ced4da;}button{cursor: pointer;border:1px solid transparent;color:#fff;background-color:#007bff;border-color:#007bff;padding:.5rem 1rem;font-size:1.25rem;line-height:1.5;border-radius:.3rem;width:100%}.form-signin{width:100%;max-width:400px;padding:15px;margin:auto;}h1{text-align: center}</style> </head> <body><main class='form-signin'> <form action='/' method='post'> <h1 class=''>Wifi Setup</h1><br/><div class='form-floating'><label>SSID</label><input type='text' class='form-control' name='ssid'> </div><div class='form-floating'><br/><label>Password</label><input type='password' class='form-control' name='password'></div><br/><br/><button type='submit'>Save</button></form></main> </body></html>" );
+  }
+}
+
+static void initializeTime()
+{
+  Serial.print("Setting time using SNTP");
+
+  configTime(-5 * 3600, 0, NTP_SERVERS);
+  time_t now = time(NULL);
+  while (now < 1510592825)
+  {
+    delay(500);
+    Serial.print(".");
+    now = time(NULL);
+  }
+  Serial.println("done!");
+}
+
+static char* getCurrentLocalTimeString()
+{
+  time_t now = time(NULL);
+  return ctime(&now);
+}
+
+static void printCurrentTime()
+{
+  Serial.print("Current time: ");
+  Serial.print(getCurrentLocalTimeString());
+}
+
+void receivedCallback(char* topic, byte* payload, unsigned int length)
+{
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+  if ((char)payload[0] == '1') {
+    digitalWrite(BUILTIN_LED, LOW);   
+  } else {
+    digitalWrite(BUILTIN_LED, HIGH);  
+  }
+}
+static void initializeClients()
+{
+  az_iot_hub_client_options options = az_iot_hub_client_options_default();
+  options.user_agent = AZ_SPAN_FROM_STR(AZURE_SDK_CLIENT_USER_AGENT);
+
+  wifi_client.setTrustAnchors(&cert);
+  if (az_result_failed(az_iot_hub_client_init(
+          &client,
+          az_span_create((uint8_t*)host, strlen(host)),
+          az_span_create((uint8_t*)device_id, strlen(device_id)),
+          &options)))
+  {
+    Serial.println("Failed initializing Azure IoT Hub client");
+    return;
+  }
+
+  mqtt_client.setServer(host, port);
+  mqtt_client.setCallback(receivedCallback);
+
+  mqtt_client.subscribe("$iothub/twin/PATCH/properties/desired/");
+}
+
+/*
+ * @brief           Gets the number of seconds since UNIX epoch until now.
+ * @return uint32_t Number of seconds.
+ */
+static uint32_t getSecondsSinceEpoch() { return (uint32_t)time(NULL); }
+
+static int generateSasToken(char* sas_token, size_t size)
+{
+  az_span signature_span = az_span_create((uint8_t*)signature, sizeofarray(signature));
+  az_span out_signature_span;
+  az_span encrypted_signature_span
+      = az_span_create((uint8_t*)encrypted_signature, sizeofarray(encrypted_signature));
+
+  uint32_t expiration = getSecondsSinceEpoch() + ONE_HOUR_IN_SECS;
+
+  // Get signature
+  if (az_result_failed(az_iot_hub_client_sas_get_signature(
+          &client, expiration, signature_span, &out_signature_span)))
+  {
+    Serial.println("Failed getting SAS signature");
+    return 1;
+  }
+
+  // Base64-decode device key
+  int base64_decoded_device_key_length
+      = base64_decode_chars(device_key, strlen(device_key), base64_decoded_device_key);
+
+  if (base64_decoded_device_key_length == 0)
+  {
+    Serial.println("Failed base64 decoding device key");
+    return 1;
+  }
+
+  // SHA-256 encrypt
+  br_hmac_key_context kc;
+  br_hmac_key_init(
+      &kc, &br_sha256_vtable, base64_decoded_device_key, base64_decoded_device_key_length);
+
+  br_hmac_context hmac_ctx;
+  br_hmac_init(&hmac_ctx, &kc, 32);
+  br_hmac_update(&hmac_ctx, az_span_ptr(out_signature_span), az_span_size(out_signature_span));
+  br_hmac_out(&hmac_ctx, encrypted_signature);
+
+  // Base64 encode encrypted signature
+  String b64enc_hmacsha256_signature = base64::encode(encrypted_signature, br_hmac_size(&hmac_ctx));
+
+  az_span b64enc_hmacsha256_signature_span = az_span_create(
+      (uint8_t*)b64enc_hmacsha256_signature.c_str(), b64enc_hmacsha256_signature.length());
+
+  // URl-encode base64 encoded encrypted signature
+  if (az_result_failed(az_iot_hub_client_sas_get_password(
+          &client,
+          expiration,
+          b64enc_hmacsha256_signature_span,
+          AZ_SPAN_EMPTY,
+          sas_token,
+          size,
+          NULL)))
+  {
+    Serial.println("Failed getting SAS token");
+    return 1;
+  }
+
+  return 0;
+}
+
+static int connectToAzureIoTHub()
+{
+  size_t client_id_length;
+  char mqtt_client_id[128];
+  if (az_result_failed(az_iot_hub_client_get_client_id(
+          &client, mqtt_client_id, sizeof(mqtt_client_id) - 1, &client_id_length)))
+  {
+    Serial.println("Failed getting client id");
+    return 1;
+  }
+
+  mqtt_client_id[client_id_length] = '\0';
+
+  char mqtt_username[128];
+  // Get the MQTT user name used to connect to IoT Hub
+  if (az_result_failed(az_iot_hub_client_get_user_name(
+          &client, mqtt_username, sizeofarray(mqtt_username), NULL)))
+  {
+    printf("Failed to get MQTT clientId, return code\n");
+    return 1;
+  }
+
+  Serial.print("Client ID: ");
+  Serial.println(mqtt_client_id);
+
+  Serial.print("Username: ");
+  Serial.println(mqtt_username);
+
+  mqtt_client.setBufferSize(MQTT_PACKET_SIZE);
+
+  while (!mqtt_client.connected())
+  {
+    time_t now = time(NULL);
+
+    Serial.print("MQTT connecting ... ");
+
+    if (mqtt_client.connect(mqtt_client_id, mqtt_username, sas_token))
+    {
+      Serial.println("connected.");
+    }
+    else
+    {
+      Serial.print("failed, status code =");
+      Serial.print(mqtt_client.state());
+      Serial.println(". Trying again in 5 seconds.");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+
+  mqtt_client.subscribe(AZ_IOT_HUB_CLIENT_C2D_SUBSCRIBE_TOPIC);
+
+  return 0;
+}
+
+
